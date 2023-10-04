5deabe2f846d7b6eb35ca176b839d526b0857398
Leonardo Carlassare <lio.del.bronx@gmail.com>
2023-10-04 12:14:05 +0200

    chore: php local server experiment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # BSZ-Impianti-Elettrici
+
+## Running a local server
+
+php -S localhost:8000
---
+++ pages/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Benvenuto</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            text-align: center;
+            margin: 0;
+            padding: 0;
+        }
+        .welcome-container {
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            padding: 20px;
+            max-width: 600px;
+            margin: 0 auto;
+        }
+        h1 {
+            color: #333;
+        }
+    </style>
+</head>
+<body>
+    <div class="welcome-container">
+        <h1>Benvenuto alla nostra pagina!</h1>
+        <p>Grazie per averci visitato.</p>
+        <p>Questa è una pagina di benvenuto di esempio creata con PHP.</p>
+    </div>
+</body>
+</html>
