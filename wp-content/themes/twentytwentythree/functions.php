5e079cb8dd015699f95d96e85c1a25bd25722251
Leonardo Carlassare <lio.del.bronx@gmail.com>
2023-10-05 20:03:58 +0200

    chore: imported files

--- wp-content/themes/twentytwentythree/functions.php
+++ wp-content/themes/twentytwentythree/functions.php
@@ -8,5 +8,13 @@ function custom_shortcode_function($atts)
     return "Questo è il mio shortcode personalizzato! Leonardo";
 }
 
+// Codice PHP personalizzato qui
+function custom_html($atts)
+{
+    // Tuo codice qui
+    return "<h2>Oggetti disponibili:</h2>";
+}
+
 add_shortcode('mio_shortcode', 'custom_shortcode_function');
+add_shortcode('my_title', 'custom_html');
 
