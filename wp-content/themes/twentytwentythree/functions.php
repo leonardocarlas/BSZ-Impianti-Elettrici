6e270afcf4e2293785c233242440d407acfe3726
Leonardo Carlassare <lio.del.bronx@gmail.com>
2023-10-05 18:32:36 +0200

    fix: removed grid component as a snipcode

--- wp-content/themes/twentytwentythree/functions.php
+++ wp-content/themes/twentytwentythree/functions.php
@@ -1,8 +1,5 @@
 <?php
 
-include_once './components/grid.php';
-
-echo Grid();
 
 // Codice PHP personalizzato qui
 function custom_shortcode_function($atts)
@@ -12,4 +9,4 @@ function custom_shortcode_function($atts)
 }
 
 add_shortcode('mio_shortcode', 'custom_shortcode_function');
-add_shortcode('items_negozio', 'Grid');
+
